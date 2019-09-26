31585a291f33f096327870abb52bc2f6382cc51b
kix2mix2 <kix2mix2@me.com>
2019-09-26 18:02:52 +0200

    The code is parallelized this is so coool OMG!

--- SepMe/main.py
+++ SepMe/main.py
@@ -1,5 +1,6 @@
-import json
 import os
+import time
+
 import click
 import mlflow
 import pandas as pd
@@ -7,25 +8,23 @@ import yaml
 from flatten_dict import flatten
 from SepMe import logger
 from SepMe.utils.graph_utils import calculate_graphs, calculate_purities
-from SepMe.utils.workflow_utils import underscore_reducer, timeit, timeit_print
+from SepMe.utils.workflow_utils import underscore_reducer, timeit
+import ray
+import psutil
+import pickle
 
+from SepMe.utils.logger import get_logger
 
 @timeit
-def save_progress(data_dict, results_path, config, i):
-    # resave results
-    # with open(results_path + config["experiment_name"] + '_' + str(i) + "_data.json", "w") as fp:
-    #     json.dump(data_dict, fp)
-
-    # flatten dict and save to dataframe
+def save_progress(data_dict, results_path):
     data_new = {}
     for file in data_dict.keys():
         data_new[file] = flatten(data_dict[file], reducer = underscore_reducer)
     results_df = pd.DataFrame.from_dict(data_new, orient = "index")
 
-
-    if os.path.exists(results_path):
-        results_all = pd.read_csv(results_path, index_col=0)
-        results_all = results_all.append(results_df, sort=False)
+    if False:
+        results_all = pd.read_csv(results_path, index_col = 0)
+        results_all = results_all.append(results_df, sort = False)
         results_all.to_csv(results_path)
 
     else:
@@ -34,90 +33,101 @@ def save_progress(data_dict, results_path, config, i):
     # return empty dict to free memory.
     return {}
 
-@timeit_print
+@ray.remote
+def process_dataset(i, file, config, class_num):
+    #logger = get_logger("SepMe_"+str(i), "../sepme_" + str(i) +'.log')
+
+    data_dict = {}
+    graph_dir = config["graph_path"] + config["experiment_name"] + "/"
+    code_name = file.split(".csv")[0] + "_cls" + str(class_num[i])
+    file_name = config["folder_path"] + code_name + ".csv"
+
+    try:
+        df = pd.read_csv(file_name, names = ["x", "y", "class"])
+    except FileNotFoundError:
+        df = None
+        #logger.info("File '" + file + "' does not exist.")
+        # data_dict[file_name] = 'n/a'
+        return
+
+    # process file
+    if df is not None:
+        with mlflow.start_run(run_name = code_name) as active_run:
+            #logger.info("Runtime: " + code_name)
+            #logger.log_param("df_size", len(df))
+
+            data_dict[code_name] = {}
+
+            graph_path = graph_dir + code_name + "_graphs.pickle"
+            nx_dict = calculate_graphs(graph_path, df, config["graph_types"])
+
+            #logger.info("Calculating purities..")
+            for graph in nx_dict.keys():
+                data_dict[code_name][graph] = {}
+                if type(nx_dict[graph]) is dict:
+                    for subgraph in nx_dict[graph].keys():
+                        data_dict[code_name][graph][subgraph] = calculate_purities(df, nx_dict[graph][subgraph],
+                                                                                   config["purities"],
+                                                                                   )
+                else:
+                    data_dict[code_name][graph] = calculate_purities(df, nx_dict[graph], config["purities"])
+
+    print('----------------- '+str(i)+' -------------------')
+    return data_dict
+
+
 @click.command()
 @click.option("--config-path", default = "SepMe/configs/config.yaml")
 @click.option("--save", default = 5)
 def workflow(config_path, save):
     # Note: The entrypoint names are defined in MLproject. The artifact directories
     # are documented by each step's .py file.
+    ray.init(num_cpus=psutil.cpu_count())
+    time.sleep(2.0)
+    start_time = time.time()
+
     with open(config_path, "r") as stream:
         try:
             config = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
+            return
 
     logger.info(config)
+    logger.info('Number of processors: '+ str(psutil.cpu_count()))
     mlflow.set_experiment(config["experiment_name"])
-    data_dict = {}
 
     # read in list of input datasets
     data_df = pd.read_csv(config["data_path"])
-
-    # read in pre-existent results
-    results_path = config["results_path"] + config["experiment_name"] + ".csv"
-    if os.path.exists(results_path):
-        results_df = pd.read_csv(results_path, index_col=0)
-    else:
-        results_df = None
+    data_df = data_df[:20]
 
     # make a directory to save graphs
     graph_dir = config["graph_path"] + config["experiment_name"] + "/"
     if not os.path.exists(graph_dir):
         os.makedirs(graph_dir)
 
-    # for each dataset
-    for i, file in enumerate(data_df.fileName):
-        code_name = file.split(".csv")[0] + "_cls" + str(data_df.classNum[i])
-        file_name = config["folder_path"] + code_name + ".csv"
+    class_num = list(data_df.classNum)
+    file_name = list(data_df.fileName)
 
-        # check if file was already processed
-        if (results_df is not None) and (code_name in results_df.index):
-            logger.info(
-                "File '"
-                + code_name
-                + "' was previously processed. Skipping..."
-            )
-            continue
+    results = []
+    for i, file in enumerate(file_name):
+        results.append(process_dataset.remote(i, file, config, class_num))
 
-        # check is file exists
-        try:
-            df = pd.read_csv(file_name, names = ["x", "y", "class"])
-        except FileNotFoundError:
-            df = None
-            logger.info("File '" + file + "' does not exist.")
-            # data_dict[file_name] = 'n/a'
-            continue
+    results = ray.get(results)
 
-        # process file
-        if df is not None:
-            with mlflow.start_run(run_name = code_name) as active_run:
-                logger.info("Runtime: " + code_name)
-                logger.log_param("df_size", len(df))
+    res_dict={}
+    for res in results:
+        res_dict.update(res)
+    results_path = config["results_path"] + config["experiment_name"] + ".csv"
+    save_progress(res_dict, results_path)
 
-                data_dict[code_name] = {}
-
-                graph_path = graph_dir + code_name + "_graphs.pickle"
-                nx_dict = calculate_graphs(graph_path, df, config["graph_types"])
-
-                logger.info("Calculating purities..")
-                for graph in nx_dict.keys():
-                    data_dict[code_name][graph] = {}
-                    if type(nx_dict[graph]) is dict:
-                        for subgraph in nx_dict[graph].keys():
-                            data_dict[code_name][graph][subgraph] = calculate_purities(df, nx_dict[graph][subgraph],
-                                                                                       config["purities"],
-                                                                                       )
-                    else:
-                        data_dict[code_name][graph] = calculate_purities(df, nx_dict[graph], config["purities"])
-
-                # every save files processed save the results
-                if i % save == 0:
-                    logger.info('Saving progress...')
-                    data_dict = save_progress(data_dict, results_path, config, i)
-
-        print('-------------------------------------')
+    end_time = time.time()
+    duration = end_time - start_time
+    logger.info("Success! The workflow took {} seconds.".format(duration))
 
 
 if __name__ == "__main__":
+    print('hello')
+
     workflow()
+
--- SepMe/utils/graph_utils.py
+++ SepMe/utils/graph_utils.py
@@ -7,89 +7,71 @@ import mlflow
 import os
 import pickle
 
-
 @timeit
 def calculate_graphs(graph_path, df, config):
     # ['del', 'ch', 'mst', 'knn', 'kncg', 'gong', 'rng', 'bt', 'as']
-
     # checks if graphs for this file were created.
     # does not check if all requested graphs are in there.
     if os.path.exists(graph_path):
-        logger.info("Graph file already exists. Reloading...")
-        with open(graph_path, "rb") as handle:
+        logger.info('Graph file already exists. Reloading...')
+        with open(graph_path, 'rb') as handle:
             return pickle.load(handle)
 
     else:
-        logger.info("Calculating Neighbourhood graphs...")
+        logger.info('Calculating Neighbourhood graphs...')
 
         graph_types = {}
 
-        if "del" in config:
-            graph_types["del"] = add_node_attr(get_delaunay(df), df)
+        if 'del' in config:
+            graph_types['del'] = add_node_attr(get_delaunay(df), df)
 
-        if "mst" in config:
-            if graph_types["del"] is not None:
-                graph_types["mst"] = get_mst(graph_types["del"])
+        if 'mst' in config:
+            if graph_types['del'] is not None:
+                graph_types['mst'] = get_mst(graph_types['del'])
             else:
-                graph_types["del"] = get_delaunay(df)
-                graph_types["mst"] = add_node_attr(
-                    get_mst(graph_types["del"]), df
-                )
+                graph_types['del'] = get_delaunay(df)
+                graph_types['mst'] = add_node_attr(get_mst(graph_types['del']), df)
 
-        if "knn" in config:
-            graph_types["knn"] = {}
-            for param in config["knn"]:
-                graph_types["knn"][param] = add_node_attr(
-                    get_knntree(df, param), df
-                )
+        if 'knn' in config:
+            graph_types['knn'] = {}
+            for param in config['knn']:
+                graph_types['knn'][param] = add_node_attr(get_knntree(df, param),df)
 
-        if "kncg" in config:
-            graph_types["kncg"] = {}
-            for param in config["kncg"]:
-                graph_types["kncg"][param] = add_node_attr(
-                    get_kncg(df, param), df
-                )
+        if 'kncg' in config:
+            graph_types['kncg'] = {}
+            for param in config['kncg']:
+                graph_types['kncg'][param] = add_node_attr(get_kncg(df, param),df)
 
-        if "gong" in config:
-            graph_types["gong"] = {}
-            for param in config["gong"]:
-                graph_types["gong"][param] = add_node_attr(
-                    get_gong(df, param), df
-                )
+        if 'gong' in config:
+            graph_types['gong'] = {}
+            for param in config['gong']:
+                graph_types['gong'][param] = add_node_attr(get_gong(df, param),df)
 
-        if "rng" in config:
-            if graph_types["del"] is None:
-                graph_types["del"] = get_delaunay(df)
-            if graph_types["mst"] is None:
-                graph_types["mst"] = graph_types["mst"] = get_mst(
-                    graph_types["del"]
-                )
-            graph_types["rng"] = {}
-            graph_types["rng"] = add_node_attr(
-                get_rng(df, graph_types["del"], graph_types["mst"]), df
-            )
+        if 'rng' in config:
+            if graph_types['del'] is None:
+                graph_types['del'] = get_delaunay(df)
+            if graph_types['mst'] is None:
+                graph_types['mst'] = graph_types['mst'] = get_mst(graph_types['del'])
+            graph_types['rng'] = {}
+            graph_types['rng'] = add_node_attr(get_rng(df, graph_types['del'], graph_types['mst']),df)
 
-        if "bt" in config:
-            graph_types["bt"] = {}
-            for param in config["bt"]:
-                graph_types["bt"][param] = add_node_attr(
-                    get_balltree(df, param), df
-                )
+        if 'bt' in config:
+            graph_types['bt'] = {}
+            for param in config['bt']:
+                graph_types['bt'][param] = add_node_attr(get_balltree(df, param),df)
 
-        if "as" in config:
-            graph_types["as"] = {}
-            for param in config["as"]:
-                graph_types["as"][param] = add_node_attr(get_as(df, param), df)
+        if 'as' in config:
+            graph_types['as'] = {}
+            for param in config['as']:
+                graph_types['as'][param] = add_node_attr(get_as(df, param),df)
 
-        if "cbsg" in config:
-            graph_types["cbsg"] = {}
-            for param in config["cbsg"]:
-                graph_types["cbsg"][param] = add_node_attr(
-                    get_CBSG(df, param), df
-                )
+        if 'cbsg' in config:
+            graph_types['cbsg'] = {}
+            for param in config['cbsg']:
+                graph_types['cbsg'][param] = add_node_attr(get_CBSG(df, param),df)
 
-        with open(graph_path, "wb") as handle:
-            pickle.dump(graph_types, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(graph_path, 'wb') as handle:
+            pickle.dump(graph_types, handle, protocol = pickle.HIGHEST_PROTOCOL)
 
         return graph_types
 
@@ -98,21 +80,15 @@ def calculate_purities(df, graph, purities):
 
     purity_dict = {}
 
-    if "mcec" in purities["type"]:
-        purity_dict["mcec"] = mcec(graph, df, 100)
-    if "ltcc" in purities["type"]:
-        purity_dict["ltcc"] = ltcc(graph, df)
+    if 'mcec' in purities['type']:
+        purity_dict['mcec'] = mcec(graph, df, 100)
+    if 'ltcc' in purities['type']:
+        purity_dict['ltcc'] = ltcc(graph, df)
 
-    neighbour_purity_list = list(
-        set(["cp", "ce", "mv"]).intersection(set(purities["type"]))
-    )
+    neighbour_purity_list = list(set(['cp', 'ce', 'mv']).intersection(set(purities['type'])))
 
     # print(total_neighbour_purity(df, graph, purities['class'], neighbour_purity_list, purities['pessimism']))
-    for pessimism in purities["pessimism"]:
-        purity_dict.update(
-            total_neighbour_purity(
-                df, graph, None, neighbour_purity_list, pessimism
-            )
-        )
+    for pessimism in purities['pessimism']:
+        purity_dict.update(total_neighbour_purity(df, graph, None, neighbour_purity_list, pessimism))
 
     return purity_dict
